a2dad7929eb8ec58bb7fad108f025ad273b09839
RichyWalter <richardwalter@web.de>
2018-09-10 08:54:15 +0200

    init Repo

---
+++ Iot_Prog_Aufagbe1.R
@@ -0,0 +1,9 @@
+#Programmierung Aufgabe-1
+#Internet der Dinge
+#Gruppe 1 
+#Oliver Graetsch, Franz Huebner,Richard Arnold, Richard Walter
+
+
+#Aufagbe 1
+#read CSV
+DF_cpu <- read.csv(file=file.choose(),head=TRUE,sep=";",stringsAsFactors=F)
